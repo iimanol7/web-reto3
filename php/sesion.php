6a71f9077a648acebb54d0497a03c3d74e627ce7
iimanol7 <149791709+iimanol7@users.noreply.github.com>
2024-04-22 21:08:09 +0200

    actualizacion

--- php/sesion.php
+++ php/sesion.php
@@ -3,28 +3,29 @@
         if($nombresesion !=null || $nombresesion !="" ){
             //creo un div con el nombre del usuario 
             echo "<div class='sesion-main'>";
-            echo "<div class='sesion' id='sesion'>";
-            echo "<img src='img/jugador.jpg' width='40px'>";
-            echo "<div class='user'> <p>".$nombresesion['nombre']."</p> <p>".$nombresesion['rol']."</p></div>";
-            //sesíon del usuario
-            echo " <div class='triangulo'></div>";
-            echo "</div>";
-
-            echo "<div class='sesion-down' id='sesion-down'>";
-                echo "<div class='card_header'>";
-                 echo "<img src='img/jugador.jpg' width='40px'>";
+                echo "<div class='sesion' id='sesion'>";
+                    echo "<img src='img/jugador.jpg' width='40px'>";
+                    echo "<div class='user'> <p>".$nombresesion['nombre']."</p> <p>".$nombresesion['rol']."</p></div>";
+                    echo " <div class='triangulo'></div>";
                 echo "</div>";
 
-                echo "<div class='card_body'>";
-                 echo "<h3>".$nombresesion['nombre']." ".$nombresesion['apellidos'] ."</h3>" ;
-                 echo "<p>".$nombresesion['email']."</p>" ;
-                 echo "<p> Rol: ".$nombresesion['rol']."</p>" ;
-                echo "</div>";
+                echo "<div class='sesion-down visible' id='sesion-down'>";
+                    echo "<div class='card_header'>";
+                        echo "<img src='img/jugador.jpg' width='70px'>";
+                        echo "<h3>".$nombresesion['nombre']."</h3>" ;
+                    echo "</div>";
 
-                echo "<div class='card_footer'>";
-                 echo "<a href='php/cerrar_sesion.php'>cerrar sesión</a>";
+                    echo "<div class='card_body'>";
+                        echo "<h4>".$nombresesion['nombre']." ".$nombresesion['apellidos'] ."</h4>" ;
+                        echo "<p>".$nombresesion['email']."</p>" ;
+                        //echo "<p class='rol'>Rol:</p>" ;
+                        echo "<p> <span>Rol:</span>".$nombresesion['rol']."</p>" ;
+                    echo "</div>";
+
+                    echo "<div class='card_footer'>";
+                        echo "<a href='php/cerrar_sesion.php'>cerrar sesión</a>";
+                    echo "</div>";
                 echo "</div>";
-           
             echo "</div>";
         
         }else{
@@ -39,7 +40,7 @@
 
             echo "<div class='sesion-down sesion-down-no' id='sesion-down'>";
            
-            echo "<a href='login.html'>Iniciar Sesión</a>";
+            echo "<a href='login.html' class='ñ'>Iniciar Sesión</a>";
             echo "</div>";
 
             echo "</div>";
